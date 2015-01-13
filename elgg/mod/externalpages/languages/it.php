7b28685598ebfe21173157920fefac6fcce4321e
Marco Colombo <molok.del.drago@gmail.com>
2015-01-13 13:31:54 +0100

    caricamento elgg installazione

---
+++ elgg/mod/externalpages/languages/it.php
@@ -0,0 +1,22 @@
+<?php
+return array(
+
+	/**
+	 * Menu items and titles
+	 */
+	'expages' => "Pagine del sito",
+	'admin:appearance:expages' => "Pagine del Sito",
+	'expages:about' => "Informazioni",
+	'expages:terms' => "Termini",
+	'expages:privacy' => "Privacy",
+	'expages:contact' => "Contatti",
+	'expages:faq' => "Domande frequenti",
+
+	'expages:notset' => "Questa pagina non e' stata ancora impostata.",
+
+	/**
+	 * Status messages
+	 */
+	'expages:posted' => "La tua pagina e' stata aggiornata con successo.",
+	'expages:error' => "Non e' possibile salvare questa pagina.",
+);
