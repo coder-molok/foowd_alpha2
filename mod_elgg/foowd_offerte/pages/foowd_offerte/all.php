74c8bc0350187c0ccf073d5e62e84c1dcc541cb3
Panciz <dav.poletti@gmail.com>
2015-02-11 22:33:44 +0100

    Esempio plugin offerte lista offerte

--- mod_elgg/foowd_offerte/pages/foowd_offerte/all.php
+++ mod_elgg/foowd_offerte/pages/foowd_offerte/all.php
@@ -1,1 +1,7 @@
 <?php
+
+
+$response = json_decode(file_get_contents('http://localhost/api_offerte/public_html/offers'));
+
+var_dump( $response->Name);
+
--- mod_elgg/foowd_offerte/start.php
+++ mod_elgg/foowd_offerte/start.php
@@ -1,1 +1,22 @@
 <?php
+
+elgg_register_event_handler('init', 'system', 'offer_init');
+
+function offer_init() {
+
+	// add a site navigation item
+	$item = new ElggMenuItem('offer', elgg_echo('offer:offers'), 'foowd_offerte/all');
+	elgg_register_menu_item('site', $item);
+
+
+	elgg_register_page_handler('foowd_offerte', 'offerte_page_handler');
+	function offerte_page_handler($segments) {
+		if ($segments[0] == 'all') {
+			include elgg_get_plugins_path() . 'foowd_offerte/pages/foowd_offerte/all.php';
+			return true;
+		}
+		return false;
+	}
+
+}
+
