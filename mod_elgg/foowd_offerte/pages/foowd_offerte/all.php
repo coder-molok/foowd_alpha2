046a5ccec76591de040b642e36df48c23c3d8e60
predorock <predorock@gmail.com>
2015-03-04 09:05:57 +0100

    Risolto baco per la gestione del page handler in elgg 1.9 . Cominciato sviluppo API

--- mod_elgg/foowd_offerte/pages/foowd_offerte/all.php
+++ mod_elgg/foowd_offerte/pages/foowd_offerte/all.php
@@ -1,7 +1,30 @@
 <?php
+// make sure only logged in users can see this page
+gatekeeper();
 
+// set the title
+// for distributed plugins, be sure to use elgg_echo() for internationalization
+$title = "Offerte Disponibili";
 
-$response = json_decode(file_get_contents('http://localhost/api_offerte/public_html/offers'));
+// start building the main column of the page
+$content = elgg_view_title($title);
+
+$response = json_decode(file_get_contents('http://localhost/foowd_project/foowd_alpha2/api_offerte/public_html/offers'));
 
 var_dump( $response);
 
+
+// add the form to this section
+$content .= $response;
+
+// optionally, add the content for the sidebar
+$sidebar = "";
+
+// layout the page
+$body = elgg_view_layout('one_sidebar', array(
+   'content' => $content,
+   'sidebar' => $sidebar
+));
+
+// draw the page
+echo elgg_view_page($title, $body);
--- mod_elgg/foowd_offerte/start.php
+++ mod_elgg/foowd_offerte/start.php
@@ -1,22 +1,32 @@
 <?php
 
+/*
+ * Foowd Offers Plugin
+ * - Custom page with offers
+ * - Menù entry to access to the offers page
+ */
+
+
 elgg_register_event_handler('init', 'system', 'offer_init');
 
 function offer_init() {
 
-	// add a site navigation item
-	$item = new ElggMenuItem('offer', elgg_echo('offer:offers'), 'foowd_offerte/all');
-	elgg_register_menu_item('site', $item);
-
-
+	//register a new page handler
 	elgg_register_page_handler('foowd_offerte', 'offerte_page_handler');
-	function offerte_page_handler($segments) {
-		if ($segments[0] == 'all') {
-			include elgg_get_plugins_path() . 'foowd_offerte/pages/foowd_offerte/all.php';
-			return true;
-		}
-		return false;
-	}
+
+	//Add a menu item to the site menu
+	elgg_register_menu_item('site', ElggMenuItem::factory(array(
+		'name' => 'offerte',
+		'href' => '/foowd_offerte/all',
+		'text' => elgg_echo('Offerte'),
+	)));
 
 }
 
+function offerte_page_handler($segments) {
+	if ($segments[0] == 'all') {
+		include elgg_get_plugins_path() . 'foowd_offerte/pages/foowd_offerte/all.php';
+		return true;
+	}
+	return false;
+}
