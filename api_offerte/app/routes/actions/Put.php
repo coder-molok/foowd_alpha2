81c0454097c98146c801dbba9a236ebdb8823cff
SimoSca <scardoni.simone@gmail.com>
2015-03-15 21:53:42 +0100

    Impostati schema.xml e composer.json. Reso compatibile il tema con Elgg 10.3 Impostato il primo prototipo per creare un'offerta

---
+++ api_offerte/app/routes/actions/Put.php
@@ -0,0 +1,99 @@
+<?php
+
+// namespace associato in composer.json via psr-4
+// in questo caso rappresenta il path a partire da actions
+
+/**
+ * classe per la creazione di nuovi record,
+ * al momento testato solo per l'inserimento di una nuova offerta
+ *
+ * I contenuti vengono ottenuti in formato json e restituiti in tale formato.
+ */
+class Put{
+
+	public $app=null;
+
+	public function __construct($app){
+
+		$this->app = $app;
+
+		// in base al parametro call associo una specifica azione.
+		// il parametro verra' impostato nei plugin Elgg.
+		$call = $app->request()->put('call');
+		$this->{$call}();
+	}
+
+	/**
+	 * per creare una nuova offerta.
+	 * E' meglio implementare la validazione direttamente tramite propel
+	 */
+	protected function offer(){
+
+		$data = json_decode($this->app->request()->put('body'));
+		//var_dump($data);
+
+		// memorizzo i singoli errori
+		$errors = array();
+		$proceed = true;
+
+		$offer = new Offer();
+		//NB: per utilizzare un processo piu' performante dovrei vedere le transiction di propel
+
+		// prima di fare il presente settaggio dovrei controllare che l'id 
+		// sia gia' associato ad un utente, altrimenti non avrebbe senso.
+		// 
+		// Provvedere una volta creata l'API per l'inserimento di un nuovo utente.
+		// 
+		$offer->setPublisher($data->publisher);
+		$offer->setDescription($data->description);
+		$offer->setName($data->name);
+
+		// operazioni sui tags
+		$tags = explode(',', $data->tags);
+		$checkTags = true;
+		$errors['tags'] = array();
+		foreach ($tags as $single) {
+			// prima di salvare, controllo che il tag sia di una sola parola
+			$single = trim($single);
+			if(preg_match('@ +@i', $single)){
+				array_push($errors['tags'], "errorone nel tag: ".$single);
+				$checkTags = false;
+				$proceed = false;
+			}
+		}
+		if($checkTags){
+			unset($errors['tags']);
+			
+			foreach($tags as $tag){
+				// dato che l'offerta viene creata una sola volta
+				// non ho bisogno di controllare la presenza dei record nella tabella offer_tags
+				$tag = new Tags();
+				$tag->setName($single);
+				$offer->addTags($tag);
+			}
+		}
+
+		// operazioni sul prezzo
+		if (preg_match('/^\d+\,\d{2,2}$/', $data->price)){
+			$offer->setPrice($data->price);
+		}else{
+			$errors['price'] = "errore nel prezzo";
+			$proceed=false;
+		}
+
+		// imposto le azioni e il ritorno
+
+		if($proceed){
+			$offer->save();
+			//echo "salvato!\n";
+			$return =json_encode(array('response'=>$proceed));
+		}else{
+			//echo "non puoi salvare";
+			$return = json_encode(array('errors'=>$errors, 'response'=>$proceed));
+		}
+
+		echo $return;
+	}
+
+
+}
--- api_offerte/public_html/index.php
+++ api_offerte/public_html/index.php
@@ -11,11 +11,37 @@
 
 #\Slim\Slim::registerAutoloader();
 
-require '../app/vendor/autoload.php';
-//require_once  '../app/generated-conf/config.php';
+
+require '/../app/vendor/autoload.php';
+
+// richiedo il file di configurazione per poter svolgere le connessioni
+require_once  '../app/data/generated-conf/config.php';
+
+// Configuro un logger
+// http://symfony.com/it/doc/current/cookbook/logging/monolog.html
+date_default_timezone_set('Europe/Rome');
+$logger = new \Flynsarmy\SlimMonolog\Log\MonologWriter(array(
+	'name' => 'FoowdLogger',
+    'handlers' => array(
+        new \Monolog\Handler\StreamHandler('../logs/'.date('Y-m-d').'.log'),
+    ),
+    'processors' => array(
+     	        	function ($record) {
+     	        		
+     				    $record['extra']['extra'] = 'Debug';
+     				    $record['context']['API'] = 'Foowe';
+     					
+     				    return $record;
+     				},
+     	        ),
+));
 
 
-$app = new \Slim\Slim();
+// avvio slim con le dovute configurazioni niziali
+$app = new \Slim\Slim(array(
+    'log.writer' => $logger,
+));
+
 
 require '../app/routes/offerte.php';
 
