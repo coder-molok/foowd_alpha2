dc25ee7d2f0a995b1a1201eb68d6370d0d9d0773
Predo <predorock@gmail.com>
2015-04-12 16:33:24 +0200

    Ho rimosso il sistema di templating Handlebars perchè andava in conflitto con require.js usato da elgg per caricare i moduli javascript. Mi sono spostato su Mustache (Handlebars si basa su questo), la sintassi rimane la stessa, ma questa template engine è disponibile anche in PHP. Ho testato con successo il funzionamento del templating system. Rimane da fare una completa integrazione del tema in elgg, e una provare a richiamare le API di Simone se tutto quadra

---
+++ mod_elgg/foowd_theme/pages/tmp/cache/mustache/__Mustache_d86e4dfd5aa2cf292315b6300d226285.php
@@ -0,0 +1,14 @@
+<?php
+
+class __Mustache_d86e4dfd5aa2cf292315b6300d226285 extends Mustache_Template
+{
+    public function renderInternal(Mustache_Context $context, $indent = '')
+    {
+        $buffer = '';
+        $newContext = array();
+
+        $buffer .= $indent . 'product';
+
+        return $buffer;
+    }
+}
--- mod_elgg/foowd_theme/start.php
+++ mod_elgg/foowd_theme/start.php
@@ -12,27 +12,22 @@ elgg_register_event_handler('init','system','foowd_theme_init');
 function foowd_theme_init() {
     
 	//Carico le librerie javascript
-	//Handlebars
-	elgg_register_js('handlebars', '/mod/foowd_theme/vendor/handlebars/handlebars.min.js');
-	elgg_load_js('handlebars');
 	//Bootstrap
 	elgg_register_js('bootstrap', '/mod/foowd_theme/vendor/bootstrap/dist/js/bootstrap.min.js');
-	elgg_load_js('bootstrap');
+	//elgg_load_js('bootstrap');
 	//JQuery
 	elgg_register_js('jquery','/mod/foowd_theme/vendor/jquery/dist/jquery.min.js');
-	elgg_load_js('jquery');
+	//elgg_load_js('jquery');
 
 	//carico librerie di style
 	//Bootstrap
 	elgg_register_css('bootstrap_css','/mod/foowd_theme/vendor/bootstrap/dist/css/bootstrap.min.css');
-	elgg_load_css('bootstrap_css');
+	//elgg_load_css('bootstrap_css');
 
 	//Carico i file javascript personalizzati
-	//Templating del prodotto
-	elgg_register_js('foowd_product','mod/foowd_theme/lib/js/product.js','footer');
-	elgg_load_js('foowd_product');
 
 	elgg_register_page_handler('', 'new_index');
+	elgg_register_page_handler('activity', 'foowd_activity_page_handler');
 }
 
 function new_index() {
@@ -41,3 +36,10 @@ function new_index() {
 
     return true;
 }
+
+function foowd_activity_page_handler() {
+	if (!include_once(dirname(__FILE__) . "/pages/wall.php"))
+        return false;
+    return true;
+}
+
