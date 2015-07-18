21d450d420494ce38e930f7b0c1fdde845577e7c
Marco Predari <predorock@gmail.com>
2015-07-18 13:46:40 +0200

    #94 maschera di nero con opacità 20% come da documento tecnico

--- mod_elgg/foowd_theme/pages/templates/templates-amd.js
+++ mod_elgg/foowd_theme/pages/templates/templates-amd.js
@@ -106,9 +106,9 @@ templates['productPost'] = template({"1":function(depth0,helpers,partials,data) 
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1, helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "<li class=\"product-post\">\n    <div class=\"product-post-image-thumbnail\">\n        <img src=\""
+  return "<li class=\"product-post\">\n    <div class=\"product-post-image-thumbnail\">\n        <figure class=\"tint\">\n            <img src=\""
     + alias3(((helper = (helper = helpers.picture || (depth0 != null ? depth0.picture : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"picture","hash":{},"data":data}) : helper)))
-    + "\">\n"
+    + "\">\n        </figure>\n"
     + ((stack1 = helpers['if'].call(depth0,(depth0 != null ? depth0.logged : depth0),{"name":"if","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
     + "        <div class=\"product-post-menu\">\n            <div onclick=\"utils.goProductDetail("
     + alias3(((helper = (helper = helpers.Id || (depth0 != null ? depth0.Id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"Id","hash":{},"data":data}) : helper)))
