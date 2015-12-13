fc3372fb1f918cff8d9dc9544f6383a3a8970687
Marco 'molok' C <molok.del.drago@gmail.com>
2015-12-14 00:49:07 +0100

    messaggi consumatore semplice a chiusura ordine

--- mod_elgg/foowd_utility/classes/Uoowd/MessageEmail.php
+++ mod_elgg/foowd_utility/classes/Uoowd/MessageEmail.php
@@ -6,12 +6,13 @@ class MessageEmail{
 
 
 	/**
-	 * Quando qualcuno si prende in carico dell'ordinazione, questo messaggio giunge a ciascun utente suo amico che aveva espresso la preferenza
-	 * la quale ora viene chiusa
+	 * Quando qualcuno si prende in carico l'ordinazione, 
+	 * questo messaggio giunge a ciascun utente suo amico 
+	 * che aveva espresso la preferenza, in attesa delle 24h
 	 * @param  [type] $ar [description]
 	 * @return [type]     [description]
 	 */
-	public function userOrderMsg($ar){
+	public function userOrderFirstMsg($ar){
 		// $ar = array();
 		// $ar['singleUsr'] ='enomis';
 		// $ar['mngrUsr'] = 'random';
@@ -20,50 +21,58 @@ class MessageEmail{
 		// $ar['ofId'] = 2;
 		// $ar['qt'] = 22;
 		// $ar['price'] = 10.23;
+		// $ar['$timeLimit'] = '18:30';
+		// $ar['$dateLimit'] = '15 dicembre (domani)';
 
 		extract($ar);
-		// prime tot e poi price!
+		// prima tot e poi price!
 		$tot = number_format($qt*$price, 2, ',', ' ');
 		$price = number_format($price, 2, ',', ' ');
 
 		$userMsgAlt ='
-		Salve %s,
+		Buongiorno %s,
 
-		il tuo amico %s si e\' preso carico di gestire la spedizione relativa all\'offerta
-		    
-		    %s
+		Stai ricevendo questa mail perche\' hai partecipato ad un ordine su foowd.it!
+		Il gruppo e\' finalmente al completo e %s si e\' reso disponibile 
+		per occuparsi del pagamento e ricevere la merce personalmente.
 
-		per la quale avevi espresso gradimento.
-		In seguito a questa operazione il sistema ha automaticamente azzerato le tue preferenze per quest\'offerta, ma puoi tranquillamente esprimerne di nuove.
-		Hai espresso:
+		Segue il riepilogo del tuo ordine: 
+		controlla la quantita\' prenotata e ricorda che hai tempo fino 
+		alle %s del %s per correggere la tua quota.
 
+		prodotto:          %s
 		preferenze: %13s 
 		a:          %13s &euro; Cad.
 		-------------------------
 		Totale:     %13s &euro;
 
-		Per maggiori dettagli devi contattare %s all\'indirizzo %s .
+    Riceverai allo scadere del tempo indicato la conferma dell\'ordine con la quota definitiva.
+		Intanto prendi contatto con %s (%s) per coordinarvi con il pagamento.
 
-		Cordialmente,
-		Foowd
+		Siamo a tua disposizione per dubbi, problemi o feedback.
+
+		Saluti da foowd, e buoni acquisti!
 		';
 		$userMsgAlt = preg_replace("@^( {4}||\t{2})@m", '', $userMsgAlt);
 		unset($ar);
-		$alt = array($singleUsr, $mngrUsr, $ofName, $qt, $price, $tot, $mngrUsr, $mngrEmail);
+		$alt = array($singleUsr, $mngrUsr, $timeLimit, $dateLimit, $ofName, $qt, $price, $tot, $mngrUsr, $mngrEmail);
 
 		$ofUrl = '<a href="'.\Uoowd\Param::offerUrl($ofId).'">'.$ofName.'</a>';
 		$userMsgHtml ='
-		Salve <b>%s</b>,<br/>
-		il tuo amico <b>%s</b> si e\' preso carico di gestire la spedizione relativa all\'offerta
-		    
-		    <p style="margin-left: 1em; font-weight: bold;">%s</p>
+		<p>Buongiorno <b>%s</b>,</p>
 
-		per la quale avevi espresso gradimento.<br/>
-		In seguito a questa operazione il sistema ha automaticamente azzerato le tue preferenze per quest\'offerta, ma puoi tranquillamente esprimerne di nuove.<br/>
-		Hai espresso:
+		<p>Stai ricevendo questa mail perche\' hai partecipato ad un ordine su foowd.it!
+		<br/>Il gruppo e\' finalmente al completo e <b>%s</b> si e\' reso disponibile 
+		per occuparsi del pagamento e ricevere la merce personalmente.</p>
 
+		<p>Segue il riepilogo del tuo ordine: 
+		<br/>controlla la quantita\' prenotata e ricorda che hai tempo fino 
+		<b>alle %s del %s</b> per correggere la tua quota.</p>
 
 		<table style="background-color: rgb(234, 228, 209); margin: 1em; border-spacing: 0.7em;"><tbody>
+			<tr>
+			<td>Prodotto:</td><td></td><td></td><td>%s</td>
+			</tr>
 			<tr>
 			<td>Preferenze:</td><td style="text-align:right;">%s</td><td>X</td>
 			</tr>
@@ -76,12 +85,106 @@ class MessageEmail{
 			</tbody>
 		</table>
 
-		Per maggiori dettagli devi contattare <b>%s</b> all\'indirizzo 
+    <p>Riceverai allo scadere del tempo indicato la conferma dell\'ordine con la quota definitiva.
+		<br/>Intanto prendi contatto con <b>%s (%s)</b> per coordinarvi con il pagamento.</p>
 
-			<p style="margin-left: 1em; font-weight: bold;">%s</p> 
+		<p>Siamo a tua disposizione per dubbi, problemi o feedback.</p>
 
-		Cordialmente,<br/>
-		Foowd
+		<p><em>Saluti da foowd, e buoni acquisti!</em></p>
+		';
+		$html = array($singleUsr, $mngrUsr, $timeLimit, $dateLimit, $ofUrl, $qt, $price, $tot ,$mngrUsr, $mngrEmail);
+
+		$tmp = new \stdClass();
+		$tmp->htmlMsg = vsprintf($userMsgHtml, $html);
+		$tmp->altMsg = vsprintf($userMsgAlt, $alt);
+
+		return $tmp;
+	}
+	/**
+	 * passate le 24h, 
+	 * questo messaggio giunge a ciascun utente in attesa 
+	 * che aveva espresso la preferenza, la quale ora viene chiusa
+	 * @param  [type] $ar [description]
+	 * @return [type]     [description]
+	 */
+	public function userOrderLastMsg($ar){
+		// $ar = array();
+		// $ar['singleUsr'] ='enomis';
+		// $ar['mngrUsr'] = 'random';
+		// $ar['mngrEmail'] = 'via@rnd.com';
+		// $ar['ofName'] = 'gran bella roba';
+		// $ar['ofId'] = 2;
+		// $ar['qt'] = 22;
+		// $ar['price'] = 10.23;
+
+		extract($ar);
+		// prima tot e poi price!
+		$tot = number_format($qt*$price, 2, ',', ' ');
+		$price = number_format($price, 2, ',', ' ');
+
+		$userMsgAlt ='
+		Buongiorno %s
+
+		Stai ricevendo questa mail da foowd.it perche\' un ordine a cui stai partecipando e\' in chiusura!
+		E\' esaurito il tempo disponibile per fare correzioni alla tua prenotazione, le quantita\' sotto riportate sono quelle definitive,
+		ora e\' sufficiente completare il pagamento tramite il tuo referente  %s  per ricevere il prodotto, che dovrai ritirare presso di lui.
+
+		Segue il riepilogo del tuo ordine: controlla la tua quota e il totale dovuto.
+
+		prodotto:          %s
+		preferenze: %13s 
+		a:          %13s &euro; Cad.
+		-------------------------
+		Totale:     %13s &euro;
+
+		Le preferenze indicate sono state gia\' azzerate su foowd.it, puoi fin da subito esprimerne di nuove.
+
+		Riceverai a breve la conferma del produttore per la disponibilita\' dei prodotti e gli estremi di pagamento,
+		MA RICORDATI che a pagare deve essere uno solo del gruppo (coordinati col tuo referente %s all\'indirizzo %s ).
+
+		Siamo a tua disposizione per dubbi, problemi o feedback.
+
+		Saluti da foowd, e buoni acquisti!
+		';
+		$userMsgAlt = preg_replace("@^( {4}||\t{2})@m", '', $userMsgAlt);
+		unset($ar);
+		$alt = array($singleUsr, $mngrUsr, $ofName, $qt, $price, $tot, $mngrUsr, $mngrEmail);
+
+		$ofUrl = '<a href="'.\Uoowd\Param::offerUrl($ofId).'">'.$ofName.'</a>';
+		$userMsgHtml ='
+		<p>Buongiorno <b>%s</b>,</p>
+
+		<p>Stai ricevendo questa mail da foowd.it perche\' un ordine a cui stai partecipando e\' in chiusura!
+		<br/>E\' esaurito il tempo disponibile per fare correzioni alla tua prenotazione, le quantita\' sotto riportate sono quelle definitive,
+		<br/>ora e\' sufficiente completare il pagamento tramite il tuo referente <b>%s</b> per ricevere il prodotto, che dovrai ritirare presso di lui.</p>
+
+		<p>Segue il riepilogo del tuo ordine: 
+		<br/>controlla la tua quota e il totale dovuto.</p>
+
+		<table style="background-color: rgb(234, 228, 209); margin: 1em; border-spacing: 0.7em;"><tbody>
+			<tr>
+			<td>Prodotto:</td><td></td><td></td><td>%s</td>
+			</tr>
+			<tr>
+			<td>Preferenze:</td><td style="text-align:right;">%s</td><td>X</td>
+			</tr>
+			<tr>
+			<td>Prezzo:</td><td style="text-align:right;">%s</td><td>&euro; Cad.</td>
+			</tr>
+			<tr style="outline: thin solid;">
+			<td>Totale:</td><td style="text-align:right;">%s</td><td>&euro;</td>
+			</tr>
+			</tbody>
+		</table>
+		
+		<strong style="color: #E9106B;">Le preferenze indicate sono state gia\' azzerate su foowd.it, puoi fin da subito esprimerne di nuove.</strong>
+
+    <p>Riceverai a breve la conferma del produttore per la disponibilita\' dei prodotti e gli estremi di pagamento,
+		<br/><strong>ma ricordati</strong> che a pagare deve essere uno solo del gruppo (<b>coordinati col tuo referente %s all\'indirizzo %s </b>).</p>
+
+		<p>Siamo a tua disposizione per dubbi, problemi o feedback.</p>
+
+		<p><em>Saluti da foowd, e buoni acquisti!</em></p>
 		';
 		$html = array($singleUsr, $mngrUsr, $ofUrl, $qt, $price, $tot ,$mngrUsr, $mngrEmail);
 
